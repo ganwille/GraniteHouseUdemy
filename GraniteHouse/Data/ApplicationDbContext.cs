cd04232f07cd503e23586414b5e1a5c277c75515
Paweł <pkskowronski@gmail.com>
2019-02-19 11:27:40 +0100

    Initial commit

---
+++ GraniteHouse/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using GraniteHouse.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraniteHouse.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProductTypes> ProductTypes { get; set; }
+    }
+}
