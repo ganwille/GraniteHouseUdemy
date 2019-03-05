7e8ba65f100ade98113c8df8d5507d51ce622b23
Paweł <pkskowronski@gmail.com>
2019-03-05 08:28:54 +0100

    Added create controller method

---
+++ GraniteHouse/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,70 @@
+﻿using GraniteHouse.Data;
+using GraniteHouse.Models.ViewModel;
+using GraniteHouse.Utility;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraniteHouse.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        [BindProperty]
+        public ProductsViewModel ProductsVM { get; set; }
+
+        public ProductsController(ApplicationDbContext db, IHostingEnvironment hostingEnvironment)
+        {
+            _db = db;
+            _hostingEnvironment = hostingEnvironment;
+            ProductsVM = new ProductsViewModel()
+            {
+                ProductTypes = _db.ProductTypes.ToList(),
+                SpecialTags = _db.SpecialTags.ToList(),
+                Products = new Models.Products()
+            };
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products =  _db.Products.Include(m => m.ProductTypes).Include(m => m.SpecialTags);
+            return View(await products.ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View(ProductsVM);
+        }
+
+        [HttpPost, ActionName("Create")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreatePost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(ProductsVM);
+            }
+
+            _db.Products.Add(ProductsVM.Products);
+            await _db.SaveChangesAsync();
+
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            var files = HttpContext.Request.Form.Files;
+
+            if(files.Count!=0)
+            {
+                var uploads = Path.Combine(webRootPath, StaticDetails.ImageFolder);
+                var extension = Path.GetExtension(files[0].FileName);
+
+                using(var filestream = new FileStream(Path.Combine(uploads,ProductsVM.Products.Id+extension),FileMode.Create))
+                {}
+            }
+        }
+    }
+}
