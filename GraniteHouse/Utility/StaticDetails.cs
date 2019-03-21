8992bfaad480837678880ef484d35ce2f95f03dd
Paweł <pkskowronski@gmail.com>
2019-03-21 20:26:42 +0100

    Added static details and create view

--- GraniteHouse/Utility/StaticDetails.cs
+++ GraniteHouse/Utility/StaticDetails.cs
@@ -7,7 +7,7 @@ namespace GraniteHouse.Utility
 {
     public class StaticDetails
     {
-        public const string DefaultProductImage = "default_product.jpg";
+        public const string DefaultProductImage = "default_product.png";
         public const string ImageFolder = @"image\ProductImage";
     }
 }
